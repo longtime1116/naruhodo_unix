9d3367492dd8792f43925bd374092c91a746b10d
shumo.tokinaga <longtime1116@gmail.com>
2020-05-08 18:27:33 +0900

    chapter9, chapter10

---
+++ process_exit.rb
@@ -0,0 +1,24 @@
+at_exit {p "finished!"}
+p "start!"
+# 引数なしだとステータスコードが0(正常終了)
+#exit
+
+# exit!はステータスコードが1
+# at_exit も実行されない
+#exit!
+
+
+# 任意の終了コードを返すこともできる
+#exit 33
+#exit! 33
+
+
+
+# abort は STDERR にメッセージを吐いた上で、at_exit を発火
+#abort "aborted!"
+
+
+
+# raise は直ちに終了するわけではなく、捕捉されなかった場合に結果としてプロセスが終了する
+# STDERR にメッセージとバックトレースが表示される
+raise "raised!!"
---
+++ process_name.rb
@@ -0,0 +1,3 @@
+# irb を起動して、↓を実行すると、プロセス名が変わる
+$PROGRAM_NAME = "irb_x"
+p $PROGRAM_NAME
